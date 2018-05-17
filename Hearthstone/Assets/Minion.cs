4addd8d3bb17f58fb0db05038b6e7014161a5e81
Macuser <macuser@ITS002815M.local>
2018-05-17 12:30:27 -0400

    Some Abstract Classes

---
+++ Hearthstone/Assets/Minion.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Minion {
+	public int health, attack, maxHealth, manaCost;
+	public string minionName, cardSet, tribe, Rarity, classCard;
+	public Ability ability;
+}
